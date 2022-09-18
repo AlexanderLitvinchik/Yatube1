307d85d844a01e363fb6ad9841cc1e1f30ba3011
Alexander Litvinchik <sasha.litvinchik@mail.ru>
2022-09-18 13:47:06 +0300

    New design

--- yatube/users/forms.py
+++ yatube/users/forms.py
@@ -1,13 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
-
+# это тоже по сути модель со своими полями , но я не знаю какими
+# насколько я понимаю это та модель которая есть у нас в posts
 User = get_user_model()
 
 
 #  создадим собственный класс для формы регистрации
 #  сделаем его наследником предустановленного класса UserCreationForm
+
+#  наследуется класс UserCreationForm:
 class CreationForm(UserCreationForm):
+    #  наследуется класс Meta, вложенный в класс UserCreationForm:
     class Meta(UserCreationForm.Meta):
         # укажем модель, с которой связана создаваемая форма
         model = User
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -12,18 +12,8 @@ from .forms import CreationForm
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("login") #  где login — это параметр "name" в path()
-    template_name = "signup.html"#  импортируем CreateView, чтобы создать ему наследника
-from django.views.generic import CreateView
-
-#  функция reverse_lazy позволяет получить URL по параметру "name" функции path()
-#  берём, тоже пригодится
-from django.urls import reverse_lazy
-
-#  импортируем класс формы, чтобы сослаться на неё во view-классе
-from .forms import CreationForm
-
-
-class SignUp(CreateView):
-    form_class = CreationForm
-    success_url = reverse_lazy("login") #  где login — это параметр "name" в path()
     template_name = "signup.html"
+
+    # form_class — из  какого класса взять форму
+    #success_url — куда  перенаправить пользователя после успешной  отправки  формы
+    #template_name — имя шаблона, куда будет передана переменная form с объектом HTML - формы.
